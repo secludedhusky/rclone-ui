3df9fc2ccdb5f7ca4b0e792119dc525f497c364f
Gary <40074095+secludedhusky@users.noreply.github.com>
2025-11-03 18:05:25 -0500

    Comment out windows_subsystem attribute

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,4 +1,4 @@
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+// #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
 	#[cfg(target_os = "linux")]
